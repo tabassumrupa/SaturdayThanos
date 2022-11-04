db7ef4089053cddd9456b5543ca4d9e1335090a0
tabassumrupa <tabassumrupa31@gmail.com>
2022-11-04 19:08:12 -0400

    New_3_1

--- src/com/upskill/assignment_3/JavaAssignment_3_1.java
+++ src/com/upskill/assignment_3/JavaAssignment_3_1.java
@@ -1,9 +1,61 @@
 package com.upskill.assignment_3;
 
+import java.util.Scanner;
+
 public class JavaAssignment_3_1 {
+	
+	String accName ;
+	float amount ;
+	boolean bool;
+	Scanner s = new Scanner (System.in);
+	
+	public JavaAssignment_3_1 (String accName){
+		this.accName = accName ;
+		System.out.println("Account name : " + this.accName);
+		System.out.println("Please enter the initial amount : " + accName + " account : ");
+		this.amount = s.nextFloat() ;
+		
+		if (this.amount <= 0){
+			bool = false;
+			System.out.println("Invalid amount! Try again with valid amount.");
+		} else  {
+			System.out.println("Initial Amount : " + this.amount);
+			bool = true ;
+		}
+		
+		while (bool == false){
+			System.out.println("Invalid amount! Try again with valid amount.");
+			System.out.println("Please enter a valid amount : " + accName + " account : ");
+			this.amount = s.nextFloat();
+			if (this.amount > 0){
+				System.out.println("Initial amount : " + this.amount);
+				bool = true;
+			}
+		}
+		
+	}
+	
+	public void deposit (float amount){
+		this.amount = this.amount + amount;
+		System.out.println("New amount after deposit : " + this.amount);
+	}
+	
+	public void withdraw (float amount){
+		if (this.amount < amount){
+			System.out.println("Insufficient Balance!");
+		} else {
+			this.amount = this.amount-amount;
+			System.out.println("New Balance after withdraw : " + this.amount);
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JavaAssignment_3_1 obj1 = new JavaAssignment_3_1 ( "Savings");
+		JavaAssignment_3_1 obj2 = new JavaAssignment_3_1 ("Checkings");
+		obj1.deposit(5000);
+		obj2.deposit(3000);
+		obj2.withdraw (1000);
+		
 
 	}
 
